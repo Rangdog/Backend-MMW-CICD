58f0a0c7560a985f26e140fdf34067597fcf8cfc
dntai2108 <162416245+dntai2108@users.noreply.github.com>
2024-06-06 09:54:43 +0700

    add api statistics
    stats top 5 product in month, import and export every month. total import and export in current month

--- base/serializers.py
+++ base/serializers.py
@@ -346,3 +346,19 @@ class ExportDetailSerializer(serializers.ModelSerializer):
 
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
+
+class TopProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    label = serializers.CharField(max_length=100)
+    quantity = serializers.IntegerField()
+    value = serializers.IntegerField()
+
+class ExportStatisticsSerializer(serializers.Serializer):
+    current = serializers.DecimalField(max_digits=20, decimal_places=2)
+    increase = serializers.DecimalField(max_digits=20, decimal_places=2)
+    percentage = serializers.DecimalField(max_digits=5, decimal_places=2)
+
+class ImportStatisticsSerializer(serializers.Serializer):
+    current = serializers.DecimalField(max_digits=20, decimal_places=2)
+    increase = serializers.DecimalField(max_digits=20, decimal_places=2)
+    percentage = serializers.DecimalField(max_digits=5, decimal_places=2)
